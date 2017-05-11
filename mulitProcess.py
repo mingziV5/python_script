72f301029407a00e606f7069e98656d55630490f
mingziV5 <ming_v5@163.com>
2017-05-11 22:18:45 +0800

    练习 mulitprocessing模块，多进程

---
+++ mulitProcess.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import multiprocessing
+
+def do_calculation(data):
+    return data*2
+
+def start_process():
+    print 'Starting',multiprocessing.current_process().name
+
+if __name__ == '__main__':
+    inputs = list(range(10))
+    print inputs
+
+    Builtin_output=map(do_calculation,inputs)
+    print Builtin_output
+
+    pool_size=multiprocessing.cpu_count()*2
+    pool=multiprocessing.Pool(processes=pool_size,initializer=start_process,)
+
+    pool_outputs=pool.map(do_calculation,inputs)
+    pool.close()
+    pool.join()
+
+    print pool_outputs
